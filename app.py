b1ecc0de3b0c48c75555d401e024097b1568450a
Chris Lindeman <slimhindrance@gmail.com>
2024-03-27 23:43:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class User(db.Model):
+    __tablename__ = 'users'  # Specifying the table name; SQLAlchemy will use the class name in lowercase if not specified
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
